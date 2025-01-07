b3633c3261873879d3cf01303caf9fd98204bd1d
geraldrolland <geraldrolland123@gmail.com>
2025-01-07 18:48:28 +0100

    update

--- src/build.sh
+++ src/build.sh
@@ -2,4 +2,4 @@ set -o errexit
 pip install -r ./src/requirements.txt
 python ./src/manage.py collectstatic --no-input
 python ./src/manage.py migrate
-sudo apt-get update && sudo apt-get install -y redis-server && systemctl start redis-server
+
